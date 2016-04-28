405203110f6b0de2fba2beee7a077bd2025587af
huro3h_satoshi <huro3h@gmail.com>
2016-04-29 07:32:58 +0800

    init commit

--- testAlert2/ViewController.swift
+++ testAlert2/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var alert1: UIButton!
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+	}
+	@IBAction func alertAction(sender: UIButton) {
+		var alertController = UIAlertController(
+			title: "大丈夫？",
+			message: "たぶんね(*´-`)",
+			preferredStyle :UIAlertControllerStyle.Alert)
+		
+		alertController.addAction(UIAlertAction(
+			title: "OK",
+			style: .Default,
+			handler: { action in print("OK")} ))
+		
+		presentViewController(alertController, animated: true, completion: nil)
+		
+		func myOK() {
+			print("OK")
+		}
 	}
 
 	override func didReceiveMemoryWarning() {
