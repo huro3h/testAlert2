8603c067c3ddbdb776ef70e98903ba623dcfdba4
huro3h_satoshi <huro3h@gmail.com>
2016-04-29 13:59:46 +0800

    Revert "OKボタンと"
    This reverts commit ef9c72efff2793087f5a6dcb3fdfaa9999f36689.

--- testAlert2/ViewController.swift
+++ testAlert2/ViewController.swift
@@ -11,21 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
 	@IBOutlet weak var alert1: UIButton!
-	@IBOutlet weak var no2: UIButton!
-	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
-	}
-	@IBAction func no2Action(sender: UIButton) {
-		var actionSheet = UIAlertController(
-			title: "たいとる",
-			message: "めっせーじ",
-			preferredStyle: UIAlertControllerStyle.ActionSheet
-		)
-		
-//		var alertController = UIAlertController(
-//		)
 	}
 	@IBAction func alertAction(sender: UIButton) {
 		var alertController = UIAlertController(
@@ -33,36 +21,23 @@ class ViewController: UIViewController {
 			message: "たぶんね(*´-`)",
 			preferredStyle :UIAlertControllerStyle.Alert)
 		
-			alertController.addAction(UIAlertAction(
+		alertController.addAction(UIAlertAction(
 			title: "OK",
 			style: .Default,
 			handler: { action in print("OK")} ))
-	
-			func myAction(){
-				print("アクション")
-
-				alertController.addAction(UIAlertAction(
-					title: "アクション1",
-					style: .Default,
-					handler: { action in self; myAction()} ))
-		
-				func myOK(){
-					print("OK")
-				}
-		
-	}
 		
 		presentViewController(alertController, animated: true, completion: nil)
 		
-
+		func myOK() {
+			print("OK")
 		}
 	}
 
-//	override func didReceiveMemoryWarning() {
-//		super.didReceiveMemoryWarning()
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
+	}
 
 
-
-
+}
 
